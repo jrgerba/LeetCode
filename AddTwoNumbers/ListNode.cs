bf08efb7cebc2a9c3d0b96d393d17f1497a81dc3
jrgerba <119472456+jrgerba@users.noreply.github.com>
2023-07-23 23:09:14 -0400

    Push Add Two Numbers

---
+++ AddTwoNumbers/ListNode.cs
@@ -0,0 +1,104 @@
+﻿using System.Security.AccessControl;
+using System.Text;
+using System.Xml;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace AddTwoNumbers;
+
+ public class ListNode 
+ {
+    public int val;
+    public ListNode next;
+
+    public override string ToString()
+    {
+        StringBuilder sb = new();
+        ListNode node = this;
+        int fullVal = 0;
+        int place = 1;
+
+        sb.Append("{ ");
+
+        while (node is not null)
+        {
+            fullVal += node.val * place;
+            place *= 10;
+
+            sb.Append(node.val);
+            sb.Append(", ");
+
+            node = node.next;
+        }
+
+        sb.Remove(sb.Length - 2, 2);
+        sb.Append(" } - ");
+        sb.Append(fullVal);
+
+        return sb.ToString();
+    }
+
+    public static bool operator ==(ListNode left, ListNode right)
+    {
+        while (left is not null && right is not null)
+        {
+            if (left.val != right.val)
+                return false;
+
+            left = left.next;
+            right = right.next;
+        }
+
+        return !(left is null ^ right is null);
+    }
+
+    public static bool operator !=(ListNode left, ListNode right)
+    {
+        return !(left == right);
+    }
+
+    public int ToInt()
+    {
+        ListNode node = this;
+        int fullVal = 0;
+
+        while (node is not null)
+        {
+            fullVal = (fullVal * 10) + node.val;
+            node = node.next;
+        }
+
+        return fullVal;
+    }
+
+    public static ListNode FromInt(int n)
+    {
+        ListNode first = null;
+        ListNode node = null;
+        while (n > 0)
+        {
+            int digit = n - ((n / 10) * 10);
+
+            if (node is null)
+            {
+                node = new ListNode(digit);
+                first = node;
+            }
+            else
+            {
+                ListNode newNode = new ListNode(digit);
+                node.next = newNode;
+                node = newNode;
+            }
+
+            n /= 10;
+        }
+
+        return first ?? new ListNode();
+    }
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+ }
---
+++ AddTwoNumbers/Solution.cs
@@ -0,0 +1,61 @@
+﻿using AddTwoNumbers;
+
+public class Solution
+{
+    public ListNode AddTwoNumbers(ListNode? l1, ListNode? l2)
+    {
+        ListNode solution = new();
+        ListNode l3 = null;
+        int carry = 0;
+
+        do
+        {
+            if (l3 is null)
+                l3 = solution;
+            else
+            {
+                l3.next = new ListNode();
+                l3 = l3.next;
+            }
+
+            l3.val = (l1?.val ?? 0) + (l2?.val ?? 0) + carry;
+            carry = l3.val / 10;
+            l3.val -= carry * 10;
+
+            l1= l1?.next;
+            l2 = l2?.next;
+        } while (l1 is not null || l2 is not null || carry != 0);
+
+        return solution;
+    }
+
+    public static void Main(string[] args)
+    {
+        ListNode[][] inputs =
+        {
+            new[] { ListNode.FromInt(342), ListNode.FromInt(465) },
+            new[] { ListNode.FromInt(0), ListNode.FromInt(0) },
+            new[] { ListNode.FromInt(9999999), ListNode.FromInt(9999) }
+        };
+        ListNode[] expectedOutputs = { ListNode.FromInt(807), ListNode.FromInt(0), ListNode.FromInt(10009998) };
+        Solution solution = new();
+
+        for (int i = 0; i < inputs.Length; i++)
+        {
+            ListNode[] input = inputs[i];
+            ListNode expected = expectedOutputs[i];
+            ListNode output = solution.AddTwoNumbers(input[0], input[1]);
+
+            if (expected == output)
+                Console.WriteLine($"Case {i} passed");
+            else
+            {
+                Console.WriteLine($"Case {i} Failed:");
+                Console.WriteLine($"\tInput 1: {input[0]}");
+                Console.WriteLine($"\tInput 2: {input[1]}");
+                Console.WriteLine($"\tOutput: {output}");
+                Console.WriteLine($"\tExpected output: {expected}");
+            }
+        }
+    }
+}
